eba1d9b91017e5139a25a085ada979407b9f6ab4
gch04721 <gch0417@naver.com>
2020-10-25 10:19:30 +0900

    temp code save

--- app/src/main/java/com/tristar/jjinbang/MainActivity.kt
+++ app/src/main/java/com/tristar/jjinbang/MainActivity.kt
@@ -2,17 +2,23 @@ package com.tristar.jjinbang
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import android.widget.FrameLayout
 import com.tristar.jjinbang.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var container: FrameLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
-        }
+        container = findViewById(R.id.fragment_container)
+
+    }
+
+    companion object{
+
     }
 }
--- app/src/main/java/com/tristar/jjinbang/ui/main/MainFragment.kt
+++ app/src/main/java/com/tristar/jjinbang/ui/main/MainFragment.kt
@@ -1,12 +1,18 @@
 package com.tristar.jjinbang.ui.main
 
+import android.graphics.Color
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.widget.SearchView
+import androidx.lifecycle.ViewModelProvider
 import com.tristar.jjinbang.R
+import kotlinx.android.synthetic.main.main_fragment.*
 
 class MainFragment : Fragment() {
 
@@ -14,17 +20,19 @@ class MainFragment : Fragment() {
         fun newInstance() = MainFragment()
     }
 
-    private lateinit var viewModel: MainViewModel
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
+
         return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+
     }
 
 }

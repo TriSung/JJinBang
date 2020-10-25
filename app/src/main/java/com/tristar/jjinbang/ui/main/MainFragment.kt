210086f8c03e59a34e19764f53e753ae18376459
gch04721 <gch0417@naver.com>
2020-10-25 13:03:12 +0900

    main fragment layout

--- app/src/main/java/com/tristar/jjinbang/ui/main/MainFragment.kt
+++ app/src/main/java/com/tristar/jjinbang/ui/main/MainFragment.kt
@@ -1,16 +1,12 @@
 package com.tristar.jjinbang.ui.main
 
 import android.graphics.Color
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
-import android.widget.TextView
-import androidx.appcompat.widget.SearchView
-import androidx.lifecycle.ViewModelProvider
 import com.tristar.jjinbang.R
 import kotlinx.android.synthetic.main.main_fragment.*
 
@@ -28,6 +24,9 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val textView = searchView_main.findViewById<EditText>(androidx.appcompat.R.id.search_src_text)
+        textView.setTextColor(Color.WHITE)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {

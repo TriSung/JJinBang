7269effd8f5ff1903eef6ec0a09454e20640d36f
gch04721 <gch0417@naver.com>
2020-10-27 18:38:43 +0900

    login message add

--- app/src/main/java/com/tristar/jjinbang/Data.kt
+++ app/src/main/java/com/tristar/jjinbang/Data.kt
@@ -1,9 +1,27 @@
 package com.tristar.jjinbang
 
+import java.util.regex.Pattern
+
 class Data{
     companion object{
         const val favoritePrefHeader : String = "FAVORITE"
         const val registerPrefHeader : String = "REGISTER"
+
+        val passReg = Regex("^(?=.*[a-zA-Z0-9])(?=.*[a-zA-Z!@#\$%^&*])(?=.*[0-9!@#\$%^&*]).{8,15}\$")
+        val idReg = Regex("^[a-zA-Z].[a-zA-Z0-9]{5,15}$")
+        val phoneNumberReg = Regex("^01[016789][0-9]{4}[0-9]{4}$")
+
+        fun passwordCheck(password: String) : Boolean{
+            return passReg.matches(password)
+        }
+
+        fun idCheck(id: String) : Boolean{
+            return idReg.matches(id)
+        }
+
+        fun phoneNumberCheck(phoneNum : String) : Boolean{
+            return phoneNumberReg.matches(phoneNum)
+        }
     }
     var favoriteRoomList: MutableList<FavoriteRoomAttribute> = mutableListOf()
     var registeredRoomList: MutableList<RoomAttributes> = mutableListOf()
--- app/src/main/java/com/tristar/jjinbang/ui/setting/login/LoginFragment.kt
+++ app/src/main/java/com/tristar/jjinbang/ui/setting/login/LoginFragment.kt
@@ -1,14 +1,23 @@
 package com.tristar.jjinbang.ui.setting.login
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.tristar.jjinbang.R
 import com.tristar.jjinbang.ui.setting.SettingFragment
 import kotlinx.android.synthetic.main.login_fragment.*
+
+/**
+ * Login
+ * Id format: need to start alphabet, using only digit , alphabet
+ * password format: need to contain a alphabet, digits, at least 8 characters
+ */
 
 class LoginFragment : Fragment() {
     companion object{
@@ -28,6 +37,31 @@ class LoginFragment : Fragment() {
 
         login_forgot.setOnClickListener { setLoginForgotBtnClickListener() }
         login_sign_up.setOnClickListener { setLoginSignUpBtnClickListener() }
+
+        login_id.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                // 입력이 끝난 경우 작동
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                // 타이핑중인 텍스트에 변화가 있으면 작동
+            }
+        })
+
+        login_password.addTextChangedListener(object: TextWatcher{
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+        })
     }
 
     private fun setLoginForgotBtnClickListener(){

2e54bb5f327ab99105ae118151cee74eb0d5369a
Sangwoo Park <sw0082@naver.com>
2020-12-01 22:01:38 +0900

    Communication final

--- app/src/main/java/com/tristar/jjinbang/ui/setting/login/Selectimage.kt
+++ app/src/main/java/com/tristar/jjinbang/ui/setting/login/Selectimage.kt
@@ -18,13 +18,17 @@ import java.io.ByteArrayOutputStream
 import java.util.*
 
 import android.app.DownloadManager
+import android.content.Context
 
 import android.content.pm.PackageManager
 
 import android.graphics.BitmapFactory
+import android.graphics.ImageDecoder
 import android.net.Uri
+import android.os.Build
 import android.util.Base64
 import android.widget.Button
+import androidx.core.graphics.drawable.toBitmap
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import com.android.volley.AuthFailureError
 import com.android.volley.Request
@@ -32,6 +36,13 @@ import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.register_room_first.*
+
+import okhttp3.Callback
+import okhttp3.ResponseBody
+import retrofit2.Call
+
+
 import java.io.InputStream
 import java.util.*
 import kotlin.collections.HashMap
@@ -60,118 +71,147 @@ class Selectimage : Fragment() {
     lateinit var bitmap: Bitmap
     lateinit var filePath : Uri
     lateinit var inputStream : InputStream
+    val cond: Int = 0
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         SelectImage_sendimage.setOnClickListener {postRequest()}
         SelectImage_imageView.setOnClickListener{openGalleryForImage()}
+
+
     }
 
     private fun openGalleryForImage() {
         val intent = Intent(Intent.ACTION_PICK)
         intent.type = "image/*"
         startActivityForResult(intent, REQUEST_GALLERY_TAKE)
+
+
     }
 
 
 
 
-    private fun postRequest(){
-        /*val bitmap: Bitmap? = imageview?.drawable?.toBitmap()
+    private fun postRequest() {
 
-        val byteArrayOutputStream = ByteArrayOutputStream()
-        if (bitmap != null) {
-            bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream)
-        }
-        val byteArray: ByteArray = byteArrayOutputStream.toByteArray()
+        if (cond == 3) {
+            val bitmap: Bitmap? = imageview?.drawable?.toBitmap()
 
-        val convertImage: String = Base64.encodeToString(byteArray, Base64.DEFAULT)
+            val byteArrayOutputStream = ByteArrayOutputStream()
+            if (bitmap != null) {
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream)
+            }
+            val byteArray: ByteArray = byteArrayOutputStream.toByteArray()
 
-        Log.e("CONVERT IMAGE : ", "" + convertImage)
+            val convertImage: String = Base64.encodeToString(byteArray, Base64.DEFAULT)
 
-        val paramObject = JSONObject()
-        paramObject.put("file", "data:image/png;base64,$convertImage")
-        paramObject.put("userName", name)
+            Log.e("CONVERT IMAGE : ", "" + convertImage)
 
-        print(paramObject)
+            val name: String = ""
+            val paramObject = JSONObject()
+            paramObject.put("file", "data:image/png;base64,$convertImage")
+            paramObject.put("userName", name)
 
-        API.service.postName(paramObject).enqueue(object : Callback<ResponseBody> {
-            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+            print(paramObject)
+
+            //API.service.postName(paramObject).enqueue(object : Callback<ResponseBody> {
+            /*fun onFailure(call: Call<ResponseBody>, t: Throwable) {
                 println("POST Throwable EXCEPTION:: " + t.message)
                 Toast.makeText(context, t.message, Toast.LENGTH_SHORT).show()
             }
-            override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
-                if (response.isSuccessful) {
-                    val msg = response.body()?.string()
+            */
+            /*fun onResponse(
+                //call: Call<ResponseBody>,
+                response: Response<ResponseBody>
+            ) {
+                if (response.isSuccess) {
+                    val msg = response
                     println("POST msg from server :: " + msg)
-                    Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+                    //Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
                 }
-            }
-        })
+            }*/
 
-        apiInteface.getTest(body, xList, yList).enqueue(object : Callback<ResponseBody> {
-            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
-                Log.d("AAA", "FAIL REQUEST ==> " + t.localizedMessage)
-                drawImageView.clear()
-            }
 
-            override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
-                Log.d("AAA", "REQUEST SUCCESS ==> ")
-                val file = response.body()?.byteStream()
-                val bitmap = BitmapFactory.decodeStream(file)
-                drawImageView.clear()
-            }
-        })
+            /*apiInteface.getTest(body, xList, yList).enqueue(object : Callback<ResponseBody> {
+                fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                    Log.d("AAA", "FAIL REQUEST ==> " + t.localizedMessage)
+                    //drawImageView.clear()
+                }
 
-        API.service.postName(paramObject).enqueue(object : Callback<ResponseBody> {
-            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
-                println("POST Throwable EXCEPTION:: " + t.message)
-                Toast.makeText(context, t.message, Toast.LENGTH_SHORT).show()
-            }
-            override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
-                if (response.isSuccessful) {
-                    val file = response.body()?.byteStream()
+                fun onResponse(
+                    //call: Call<ResponseBody>,
+                    response: Response<ResponseBody>
+                ) {
+                    Log.d("AAA", "REQUEST SUCCESS ==> ")
+                    val file = response.result?.byteStream()
                     val bitmap = BitmapFactory.decodeStream(file)
-                    val msg = response.body()?.string()
-                    println("POST msg from server :: " + msg)
-                    Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+                    //drawImageView.clear()
+                }
+            })*/
+
+            //API.service.postName(paramObject).enqueue(object : Callback<ResponseBody> {
+            fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                println("POST Throwable EXCEPTION:: " + t.message)
+                Toast.makeText(context, t.message, Toast.LENGTH_SHORT).show()
+            }
+
+            fun onResponse(
+                //call: Call<ResponseBody>,
+                response: Response<ResponseBody>
+            ) {
+                if (response.isSuccess) {
+                    //val file = response.body()?.byteStream()
                 }
             }
-        })
 
-         */
-        val stringRequest = object : StringRequest(Request.Method.POST, postURL,
-            Response.Listener<String> { response ->
-                Toast.makeText(context, response, Toast.LENGTH_LONG).show()
-            }, Response.ErrorListener { error->
-                Toast.makeText(context, "Error: ${error.toString()}", Toast.LENGTH_LONG).show()
-            }){
-            override fun getParams(): HashMap<String, String> {
-                val params = HashMap<String, String>()
-                val imageData = imageToString(bitmap)
-                params.put("image", imageData)
-                return params
+
+            val stringRequest = object : StringRequest(Request.Method.POST, postURL,
+                Response.Listener<String> { response ->
+                    Toast.makeText(context, response, Toast.LENGTH_LONG).show()
+                }, Response.ErrorListener { error ->
+                    Toast.makeText(context, "Error: ${error.toString()}", Toast.LENGTH_LONG).show()
+                }) {
+                override fun getParams(): HashMap<String, String> {
+                    val params = HashMap<String, String>()
+                    //val imageData = imageToString(bitmap)
+                    //params.put("image", imageData)
+                    return params
+                }
             }
+            //requestQueue.add(stringRequest)
+
+
+
+
+
+
+
         }
-
-        requestQueue = Volley.newRequestQueue(this.)
-        requestQueue.add(stringRequest)
-
-
-
     }
 
 
-    private fun imageToString(bitmap: Bitmap):String {
+    fun selectImage() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
+            type = "image/*"
+        }
+        if (intent.resolveActivity(requireActivity().packageManager) != null) {
+            this.startActivityForResult(intent, REQUEST_GALLERY_TAKE)
+        }
+    }
 
-        val outputStream = ByteArrayOutputStream()
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
-        imageBytes = outputStream.toByteArray()
-
-        encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT)
-
-        return encodedImage
+    fun getBitmap(context: Context, imageUri: Uri): Bitmap ? {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            ImageDecoder.decodeBitmap(
+                ImageDecoder.createSource(
+                    context.contentResolver,
+                    imageUri))
+        } else {
+            context.contentResolver.openInputStream(imageUri) ?.use {
+                    inputStream ->
+                BitmapFactory.decodeStream(inputStream)
+            }
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -190,6 +230,7 @@ class Selectimage : Fragment() {
                         SelectImage_imageView.setImageURI(uri)
                     }
                 }
+                Toast.makeText(context, "${filePath.toString()}", Toast.LENGTH_LONG).show()
             }
         }
     }
